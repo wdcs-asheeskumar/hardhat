c9a701baab79c266d3d3cfea1f2847a7d8d2c67c
wdcs-asheeskumar <154244737+wdcs-asheeskumar@users.noreply.github.com>
2024-02-22 15:19:37 +0530

    Update hardhat.config.js

--- hardhat_project/hardhat.config.js
+++ hardhat_project/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_API_KEY = "L_ntEghTGnxs6jMZJBtp8r3oODPJdTPE";
+
+const ALCHEMY_PRIVATE_KEY = "87af2e04beb86e2d93f128d3816c966a8f3e821e7b3f5f9816b453a6c0328b81";
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${ALCHEMY_PRIVATE_KEY}`] 
+    }
+  }
 };
