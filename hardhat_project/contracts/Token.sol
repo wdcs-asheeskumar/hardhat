00e993b4bf6c63e587d24653423d60c218bdb98a
wdcs-asheeskumar <ashees.kumar@codezeros.com>
2024-02-22 14:39:06 +0530

    hardhat

---
+++ hardhat_project/contracts/Token.sol
@@ -0,0 +1,38 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token {
+    string public name = "My Hardhat Token";
+    string public symbol = "MHT";
+
+    uint256 public totalSupply = 1000000;
+
+    address public owner;
+
+    mapping(address => uint256) balances;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event approvalAmount(address owner, address spender, uint value);
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
